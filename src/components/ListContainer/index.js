40ac7e544c36678dac9fe9a0058bc55fdcdf6160
AnsonTu <anson9137@gmail.com>
2021-01-10 05:16:30 -0500

    Fixed styling

--- src/components/ListContainer/index.js
+++ src/components/ListContainer/index.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 import Alert from "@material-ui/lab/Alert";
 import MoviesList from "./MoviesList";
 import NomineesList from "./NomineesList";
@@ -9,7 +9,8 @@ import NomineesList from "./NomineesList";
 const useStyle = makeStyles({
   paper: {
     width: "1000px",
-    height: "550px"
+    height: "550px",
+    marginTop: "40px"
   },
   alert: {
     width: "900px",
@@ -20,35 +21,32 @@ const useStyle = makeStyles({
 
 const ListContainer = (props) => {
   const classes = useStyle();
-  const { movies } = props;
-  const [nominees, setNominees] = useState([]);
+  const { movies, nominees, setNominees } = props;
 
   return (
-    <Grid item container xs={12} justify="center">
-      <Paper className={classes.paper} elevation={12}>
-        <Grid container justify="center">
-          {nominees.length < 5 ? (
-            <Alert className={classes.alert} severity="info">
-              {5 - nominees.length} more movies can be nominated!
-            </Alert>
-          ) : (
-            <Alert className={classes.alert} severity="success">
-              5 movies have been nominated!
-            </Alert>
-          )}
-          <Grid item container sm={6} alignItems="center" justify="center">
-            <MoviesList
-              movies={movies}
-              nominees={nominees}
-              setNominees={setNominees}
-            />
-          </Grid>
-          <Grid item container sm={6} alignItems="center" justify="center">
-            <NomineesList nominees={nominees} setNominees={setNominees} />
-          </Grid>
+    <Paper className={classes.paper} elevation={12}>
+      <Grid container justify="center">
+        {nominees && nominees.length < 5 ? (
+          <Alert className={classes.alert} severity="info">
+            {5 - nominees.length} more movies can be nominated!
+          </Alert>
+        ) : (
+          <Alert className={classes.alert} severity="success">
+            5 movies have been nominated!
+          </Alert>
+        )}
+        <Grid item container sm={6} alignItems="center" justify="center">
+          <MoviesList
+            movies={movies}
+            nominees={nominees}
+            setNominees={setNominees}
+          />
         </Grid>
-      </Paper>
-    </Grid>
+        <Grid item container sm={6} alignItems="center" justify="center">
+          <NomineesList nominees={nominees} setNominees={setNominees} />
+        </Grid>
+      </Grid>
+    </Paper>
   );
 };
 
