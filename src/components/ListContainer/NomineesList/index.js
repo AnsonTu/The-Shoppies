e2299326cce8e1d45ff1c48e46554626403cf749
AnsonTu <anson9137@gmail.com>
2021-01-14 18:34:41 -0500

    Add button for clearing nominees list

--- src/components/ListContainer/NomineesList/index.js
+++ src/components/ListContainer/NomineesList/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
-import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import ClearAllIcon from "@material-ui/icons/ClearAll";
+import Divider from "@material-ui/core/Divider";
 import NomineeCard from "../NomineeCard";
 
 const useStyle = makeStyles({
@@ -16,6 +18,11 @@ const useStyle = makeStyles({
     fontSize: "18px",
     margin: "8px 0 8px 10px"
   },
+  clearButton: {
+    textTransform: "none",
+    backgroundColor: "#F29274"
+  },
+  clearIcon: { marginRight: "6px", color: "#7D2105" },
   cardContainer: {
     maxHeight: "400px"
   }
@@ -25,9 +32,19 @@ const NomineesList = (props) => {
   const classes = useStyle();
   const { nominees, setNominees, handleOpenModal, handleNewPoster } = props;
 
+  const handleClearNominees = () => {
+    setNominees([]);
+  };
+
   return (
     <Paper className={classes.paper} variant="outlined">
-      <Typography className={classes.title}>List of Nominees</Typography>
+      <Grid container justify="space-between">
+        <Typography className={classes.title}>List of Nominees</Typography>
+        <Button className={classes.clearButton} onClick={handleClearNominees}>
+          <ClearAllIcon className={classes.clearIcon} />
+          <Typography>Clear List</Typography>
+        </Button>
+      </Grid>
       <Divider />
       <Grid container justify="center" style={{ overflowY: "auto" }}>
         <div className={classes.cardContainer}>
